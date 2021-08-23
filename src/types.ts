6e164e6e3e9b8e7d3c2d39d4cccc0fc3bc7e998a
Nathan Merritt <nathan.merritt@onboarddata.io>
2021-08-23 10:44:04 -0600

    remove unused path option

--- src/types.ts
+++ src/types.ts
@@ -12,9 +12,7 @@ export const defaultQuery: Partial<MyQuery> = {
 /**
  * These are options configured for each DataSource instance
  */
-export interface MyDataSourceOptions extends DataSourceJsonData {
-  path?: string;
-}
+export interface MyDataSourceOptions extends DataSourceJsonData {}
 
 /**
  * Value that is used in the backend, but never sent over HTTP to the frontend
